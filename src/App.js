22c3baee38efa38f33a7805275ebc5a4a7eceb93
Nghia <binlong09@gmail.com>
2018-04-22 00:18:31 +0700

    Before redux-thunk

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 import reducers from './reducers';
+import LoginForm from './components/LoginForm';
+import ReduxThunk from 'redux-thunk'; //middleware
 
 class App extends Component {
+
+	componentWillMount() {
+		var config = {
+			apiKey: 'AIzaSyAWeGeMIt2En-cDBqoy9R1LwNwv8PGDFJA',
+			authDomain: 'manager-1613c.firebaseapp.com',
+			databaseURL: 'https://manager-1613c.firebaseio.com',
+			projectId: 'manager-1613c',
+			storageBucket: 'manager-1613c.appspot.com',
+			messagingSenderId: '597230856570'
+		};
+
+		firebase.initializeApp(config);
+	}
+
   render() {
     return (
 			<Provider store={createStore(reducers)}>
 				<View>
-					<Text>
-						Hello
-					</Text>
+					<LoginForm />
 				</View>	
 			</Provider>
     );
---
+++ src/actions/types.js
@@ -0,0 +1,2 @@
+export const EMAIL_CHANGED = 'email_changed'; // this string does not have to match variable name. No big difference between lower case and uppercase
+export const PASSWORD_CHANGED = 'password_changed';
