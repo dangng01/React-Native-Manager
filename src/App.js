756a80d53fa503af0a4fdcfc53624351564ca212
Nghia <binlong09@gmail.com>
2018-04-22 00:20:06 +0700

    Wire Redux-thunk

--- src/App.js
+++ src/App.js
@@ -22,8 +22,11 @@ class App extends Component {
 	}
 
   render() {
+
+		const store = createStore(reducers, {}, applyMiddleware(ReduxThunk))
+
     return (
-			<Provider store={createStore(reducers)}>
+			<Provider store={store}>
 				<View>
 					<LoginForm />
 				</View>	
